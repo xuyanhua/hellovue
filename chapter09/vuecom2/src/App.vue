843c67ee0c983d49aa9b3a1599dd78618ce45987
xuyh <xuyh@missfresh.cn>
2020-09-08 19:03:36 +0800

    单页面组件

--- chapter09/vuecom2/src/App.vue
+++ chapter09/vuecom2/src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <test text="xxii"></test>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <News title="xxii" content="123121212"></News>
+    <Title title="这是一个标题组件"></Title>
+    <Button color="red" @click="handleClick()">这是按钮文本</Button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import test from './components/test.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import News from "./components/News";
+import Title from "./components/Title";
+import Button from "./components/Button";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
-    test
-  }
-}
+    News,
+    Title,
+    Button,
+  },
+  methods: {
+    handleClick() {
+      console.log("hhhhhh");
+    },
+  },
+};
 </script>
 
 <style>
---
+++ chapter09/vuecom2/src/components/News.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>{{title}}</h2>
+    <div class="content">{{content}}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "test",
+  props: {
+    title: {
+      type: String,
+      default: "hahaha",
+    },
+    content: {
+      type: String,
+      default: "hahaha",
+    },
+  },
+};
+</script>
+<style scoped>
+.content {
+  color: red;
+  font-size: 24px;
+}
+</style>
