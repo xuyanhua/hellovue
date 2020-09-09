40dc5d0268526c72891ecdaf10ce67b884e08c08
xuyh <xuyh@missfresh.cn>
2020-09-09 08:05:39 +0800

    vue router

---
+++ chapter10/vuer/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    meta: { title: '首页' },
+    component: Home
+    // component: (resolve) => require(['../views/Home.vue'], resolve)
+  },
+  {
+    path: '/about',
+    name: 'About',
+    meta: { title: '关于' },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+router.beforeEach((to, from, next) => {
+  window.document.title = to.meta.title;
+  next();
+})
+
+export default router
